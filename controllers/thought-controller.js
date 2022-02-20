146e90e631831d9e34ca48bb97a9ba859c5bbc03
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-02-20 14:55:47 -0600

    added thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -107,5 +107,24 @@ const thoughtController = {
                 console.log(err);
                 res.status(400).json(err);
             });
+    },
+
+    updateThought({params, body}, res) {
+        Thought.findOneAndUpdate({_id: params.id}, body, {
+            new: true
+        })
+        .then((dbThoughtData) => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'No thought found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     }
 }
+
+module.exports = thoughtController
---
+++ routes/api-routes/thought-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getThoughtById,
+    addThought,
+    addReaction,
+    removeReaction,
+    removeThought,
+    updateThought
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(addThought)
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(removeThought)
+
+router.route('/:thoughtId/reactions').post(addReaction)
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
+
+module.exports = router;
